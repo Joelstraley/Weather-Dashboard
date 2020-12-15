805906c78491fbbbe4f6bc810888102a2ac308ac
Joel Straley <joelstraley@gmail.com>
2020-12-15 18:48:48 -0500

    setting up city search function

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?&appid=" + apiKey + "&q=" + queryTerm;
 var apiKey = "c4ca0a8bcd276697a319df840918bfae";
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?&appid=" + apiKey; 
 
 var queryTerm = ""
 
@@ -17,15 +17,30 @@ function runQuery(queryURL){
     })
 }
 
+function(OWData) {
+
+}
+
 /* function clear() {
     $("#weatherblock").empty();
   } */
 
 
-$('#citySearch').on('click', function(event){
-   alert("test");
-   console.log(queryURL)
-    runQuery("http://api.openweathermap.org/data/2.5/weather?&appid=c4ca0a8bcd276697a319df840918bfae&q=london");
+$('#searchBtn').on('click', function(event){
+//records User Input and fixes spaces and cuts off unnecessary end spaces 
+   var searchInput = $("#searchInput").val().replace(" ","%20").trim();
+//Puts the User Input into API URL for Ajax call 
+    var newURL = queryURL + "&q=" + searchInput;
+//Send AJAX call the URL with user inputted city 
+    runQuery(newURL)
+
+cityName = $("#activeCity").val().trim();
+temp = $('#temperature').val().trim();
+humidity = $('#humidity').val().trim();
+wind = $('#wind-speed').val().trim();
+UVindex = ($"#UVindex").val().trim(); 
+
+
     return false;
 });
 
