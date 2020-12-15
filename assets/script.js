aa0a2059d27198bf66d4f3f6042143e4147d615e
Joel Straley <joelstraley@gmail.com>
2020-12-15 16:48:21 -0500

    listed psuedo-code into JS doc

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+var mainURL = "api.openweathermap.org/data/2.5/weather?q="
+var api = c4ca0a8bcd276697a319df840918bfae
+
+
+
+//1. take citySearch input and do AJAX call to main OpenWeather API
+    // - api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+ //2. Pull elements for Weather Object to insert into appropriate places in City Block area
+    // pull UV information from UV index API
+    //set CSS class to be affected by UV index information 
+//3. Pulling from 5 day forecast Weather Object to load info into HTML Cards
+//4. save Searched City into local storage so it reloads upon refresh. 
+
+
+
