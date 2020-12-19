0aae0e1b399bcc7f696e03e134b8a8049d39b715
Joel Straley <joelstraley@gmail.com>
2020-12-19 10:07:43 -0500

    weather icon for 5day forecast solved

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+var searchInput 
 var cityName = $('#activeCity'); 
 var leftColumn = $('#leftColumn');
 var wIcon = $('#wIcon')
@@ -28,8 +29,6 @@ var day5date = $('#day5date');
 var day5temp = $('#day5temp');
 var day5humidity = $('#day5humidity');
 var day5icon = $('#wIcon5');
-
-
 
 var apiKey = "c4ca0a8bcd276697a319df840918bfae";
 var blockURL = "http://api.openweathermap.org/data/2.5/weather?&appid=" + apiKey + "&units=imperial"; 
@@ -78,51 +77,46 @@ function runForecast(forecastURL){
     method: "GET"})
     .then(function(forecastData){    
        // day1date.text((parseInt(forecastData.list[0].dt) * 1000).toLocaleSting("en-US", {timeZoneName: "short"}));
-        day1date.text((dayjs((forecastData.list[0].dt * 1000)).format('M/DD/YYYY')));    
+        day1date.text((dayjs((forecastData.list[0].dt * 1000)).format('M/DD/YY')));    
         day1temp.text(forecastData.list[0].main.temp.toFixed(0) + "℉");
-        console.log(forecastData.list[0].main.humidity);
         day1humidity.text(forecastData.list[0].main.humidity + "%");
 
-        console.log(forecastData.list[0].weather[0]);
-        day1icon.attr('src', "http://openweathermap.org/img/w/" + forecastData.list[0].weather.icon + ".png");
+        console.log(forecastData.list[0].weather[0].icon);
+        day1icon.attr('src', "http://openweathermap.org/img/w/" + forecastData.list[0].weather[0].icon + ".png");
        
-        day2date.text((dayjs((forecastData.list[9].dt * 1000)).format('M/DD/YYYY')));    
+        day2date.text((dayjs((forecastData.list[9].dt * 1000)).format('M/DD/YY')));    
         day2temp.text(forecastData.list[9].main.temp.toFixed(0) + "℉");
         day2humidity.text(forecastData.list[9].main.humidity + "%");
         day2icon.attr('src', "http://openweathermap.org/img/w/" + forecastData.list[9].weather.icon + ".png");
 
-        day3date.text((dayjs((forecastData.list[17].dt * 1000)).format('M/DD/YYYY')));    
+        day3date.text((dayjs((forecastData.list[17].dt * 1000)).format('M/DD/YY')));    
         day3temp.text(forecastData.list[17].main.temp.toFixed(0) + "℉");
         day3humidity.text(forecastData.list[17].main.humidity + "%");
         day3icon.attr('src', "http://openweathermap.org/img/w/" + forecastData.list[17].weather.icon + ".png");
         
-        day4date.text((dayjs((forecastData.list[25].dt * 1000)).format('M/DD/YYYY')));    
+        day4date.text((dayjs((forecastData.list[25].dt * 1000)).format('M/DD/YY')));    
         day4temp.text(forecastData.list[25].main.temp.toFixed(0) + "℉");
         day4humidity.text(forecastData.list[25].main.humidity + "%");
         day4icon.attr('src', "http://openweathermap.org/img/w/" + forecastData.list[25].weather.icon + ".png");
     
-        day5date.text((dayjs((forecastData.list[33].dt * 1000)).format('M/DD/YYYY')));    
+        day5date.text((dayjs((forecastData.list[33].dt * 1000)).format('M/DD/YY')));    
         day5temp.text(forecastData.list[33].main.temp.toFixed(0) + "℉");
         day5humidity.text(forecastData.list[33].main.humidity + "%");
-        day5icon.attr('src', "http://openweathermap.org/img/w/" + forecastData.list[33].weather.icon + ".png");
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+        day5icon.attr('src', "http://openweathermap.org/img/w/" + forecastData.list[33].weather.icon + ".png");    
+  
+   
     })
 }
 
 
+
+
+
+
+
 $('#searchBtn').on('click', function(event){
+    searchInput = $("#searchInput").val().replace(" ","%20").trim();
     //records User Input and fixes spaces and cuts off unnecessary end spaces 
-       var searchInput = $("#searchInput").val().replace(" ","%20").trim();
     //Puts the User Input into API URL for Ajax call 
         var newURL = blockURL + "&q=" + searchInput;
     //Send the URL with user inputted city to a function to pull Ajax call 
@@ -135,9 +129,19 @@ $('#searchBtn').on('click', function(event){
 
  //Function to save Unser Input into local storage // 
 function setInput() {
-    var citySearch = $(".list-group").html(`"<li class="list-group-item">{searchInput}</li>"`);
-    localStorage.setItem(citySearch);
+    var citySearch = $(".list-group").html(`<li class="list-group-item">${searchInput}</li>`);
+    localStorage.setItem("cities", searchInput);
  };
+
+
+
+/// push searchInput into an empty array, set that to local storage and JSON.Parse, loop through array
+// and for each that keeps adding:    
+// $(".list-group").append(`<li class="list-group-item">${searchInput}</li>`);
+
+
+
+
 
 //Function to retreive local storage input values// 
 /*  function save(){
@@ -153,7 +157,6 @@ function setInput() {
 
     // pull UV information from UV index API
     //set CSS class to be affected by UV index information 
-//3. Pulling from 5 day forecast Weather Object to load info into HTML Cards
 //4. save Searched City into local storage so it reloads upon refresh. 
 
 
